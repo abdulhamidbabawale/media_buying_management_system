df97d6015ad3d0335884a273134fe9bd87ba4061
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-09-15 14:47:54 +0100

    feat:kept only db queiries and removed business logic

--- app/db/client_queries.py
+++ app/db/client_queries.py
@@ -8,16 +8,10 @@ from fastapi.encoders import jsonable_encoder
 
 collection = db.clients
 
-async def create_client(client: Client):
-    client_dict = jsonable_encoder(client)
-    try:
-        result = await collection.insert_one(client_dict)
-        if result.inserted_id:  # Successful insertion
-            return {"success": True, "client_id": str(result.inserted_id)}
-        else:
-            return {"success": False, "error": "Insertion failed"}
-    except PyMongoError as e:  # Catch MongoDB errors
-        return {"success": False, "error": str(e)}
+
+async def create_client(client: dict):
+    result =await collection.insert_one(client)
+    return str(result.inserted_id)
 
 async def get_client_by_id(client_id: str):
     return await collection.find_one({"_id": client_id})
