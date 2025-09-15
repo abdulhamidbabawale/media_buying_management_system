dee5b9bf15b4d0d7cb824d1513704327e0d6d9ac
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-09-15 14:57:53 +0100

    chore:jwt config

---
+++ app/jwt.py
@@ -0,0 +1,38 @@
+from datetime import datetime,timedelta
+from jose import JWTError, jwt
+from pydantic import BaseModel
+from starlette import status
+from passlib.context import CryptContext
+from app.config import settings
+
+
+SECRET_KEY = settings.JWT_SECRET_KEY
+ALGORITHM = 'HS256'
+
+ACCESS_TOKEN_EXPIRE_MINUTES =  settings.JWT_ACCESS_TOKEN_EXPIRE_MINUTES
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
+
+
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
