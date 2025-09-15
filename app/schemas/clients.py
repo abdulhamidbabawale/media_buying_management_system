6cb89a180c542bfb995a5780051f6b071020f522
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-09-15 14:53:37 +0100

    refractor(client):commented

--- app/schemas/clients.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class ClientCreateResponse(BaseModel):
-    message: str
-    client_id: str
--- app/schemas/clients_schema.py
+++ app/schemas/clients_schema.py
@@ -4,6 +4,8 @@ class ClientCreateResponse(BaseModel):
     message: str
     client_id: str
 
+"""response for fetching client details"""
 class ClientFetchResponse(BaseModel):
     message: str
     data: object
+"""response for fetching client details"""
