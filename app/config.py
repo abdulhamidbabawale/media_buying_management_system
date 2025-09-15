77371b83788856acee25277e6d4c2019f835bb2f
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-09-15 14:49:06 +0100

    chore:added jwt config

--- app/config.py
+++ app/config.py
@@ -9,6 +9,9 @@ class Settings:
     DB_NAME: str = os.getenv("DB_name")
     SECRET_KEY: str = os.getenv("SECRET_KEY", "supersecret")
     DEBUG: bool = os.getenv("DEBUG", "True") == "True"
+    JWT_SECRET_KEY: str = os.getenv("JWT_SECRET_KEY")
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES = 15
+    JWT_REFRESH_TOKEN_EXPIRE_DAYS = 7
 
 # Create a single instance for use across the app
 settings = Settings()
