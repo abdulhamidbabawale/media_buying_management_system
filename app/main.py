c835090b62bdc948ba08b24598e509caa541c692
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-09-17 18:41:52 +0100

    fix:redis port

--- app/main.py
+++ app/main.py
@@ -19,8 +19,7 @@ app.include_router(skus.router, prefix="/api/v1")
 @app.on_event("startup")
 async def startup():
     # Connect to Redis
-    redis_url = os.environ.get("REDIS_URL", "redis://redis:6379/0")
-    r = redis.from_url(redis_url, encoding="utf8", decode_responses=True)
+    r = redis.from_url("redis://localhost:6379", encoding="utf-8", decode_responses=True)
     await FastAPILimiter.init(r)
 
 @app.on_event("shutdown")
