d03a3934f2528ed70a94fcd21253c2f97fa33f7b
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-09-17 08:03:29 +0100

    feat:added sku route and and changed redis setup

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI,Depends,status
-from app.routers import clients,auth
+from app.routers import clients,auth,skus
 from fastapi_limiter import FastAPILimiter
 from fastapi_limiter.depends import RateLimiter
 import redis.asyncio as redis
+import uvicorn
+import os
 
 app = FastAPI(title="Media Buying Management System",dependencies=[Depends(RateLimiter(times=100, seconds=60))])
 
 # Register routers
 app.include_router(clients.router, prefix="/api/v1")
 app.include_router(auth.router, prefix="/api/v1")
+app.include_router(skus.router, prefix="/api/v1")
 
 @app.on_event("startup")
 async def startup():
     # Connect to Redis
-    r = redis.from_url("redis://localhost:6379", encoding="utf-8", decode_responses=True)
+    redis_url = os.environ.get("REDIS_URL", "redis://redis:6379/0")
+    r = redis.from_url(redis_url, encoding="utf8", decode_responses=True)
     await FastAPILimiter.init(r)
 
 @app.on_event("shutdown")
