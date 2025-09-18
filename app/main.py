6e7869705e1514bae356eda2b9ee3cca536aa756
abdulhamidbabawale <hamidbabawale@gmail.com>
2025-09-18 01:57:05 +0100

    fix:set redis url

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-app = FastAPI(title="Media Buying Management System")
+app = FastAPI(title="Media Buying Management System",dependencies=[Depends(RateLimiter(times=100, seconds=60))])
 
 env= os.getenv("ENVIRONMENT")
 
@@ -18,7 +18,17 @@ app.include_router(clients.router, prefix="/api/v1")
 app.include_router(auth.router, prefix="/api/v1")
 app.include_router(skus.router, prefix="/api/v1")
 
+@app.on_event("startup")
+async def startup():
+    # Connect to Redis
 
+    redis_url = os.environ.get("REDIS_URL", "redis://redis:6379")
+    r = redis.from_url(redis_url, encoding="utf8", decode_responses=True)
+    await FastAPILimiter.init(r)
+
+@app.on_event("shutdown")
+async def shutdown():
+    await FastAPILimiter.close()
 
 @app.get("/")
 def home():
